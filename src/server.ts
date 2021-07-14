ce5dafc726f84accf495760585dfcdc70485e984
jsGolden <foolycoolyvip@gmail.com>
2021-07-14 13:34:49 -0300

    Express initial route [GET '/': Hello World]! :rocket:

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
+
+app.listen(3333, () => console.log('🚀 Backend running on port 3333!'));
