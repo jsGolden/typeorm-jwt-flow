5fc8bd84f1b1e048c193aed6c6f7008582e46719
jsGolden <foolycoolyvip@gmail.com>
2021-07-14 13:38:21 -0300

    App migrated to app.ts

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,3 @@
-import express from 'express';
-import cors from 'cors';
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' });
-});
+import app from './app';
 
 app.listen(3333, () => console.log('🚀 Backend running on port 3333!'));
